9ea054309e169c4bdedb98a713eb0f63a8f42393
Josh Miller <notjoshmiller@gmail.com>
2014-11-30 18:32:07 -0600

    Use watchify properly and add images to watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,14 +2,12 @@ var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 
 // Watch for changes
-gulp.task('watch', ['serve'], function (next) {
+gulp.task('watch', ['setWatch', 'serve'], function (next) {
     livereload.listen();
-    global.isWatching = true;
 
-    gulp.watch(['./src/scripts/**/*.js', './src/index.html'], ['browserify']);
-    gulp.watch(['./src/styles/**/*.less'], ['styles']);
+    gulp.watch('./src/styles/**/*.less', ['styles']);
+    gulp.watch('./src/images/**', ['images']);
     gulp.watch('bower.json', ['wiredep']);
-
     gulp.watch('./.tmp/**').on('change', function (file) {
         livereload.changed(file.path);
     });
