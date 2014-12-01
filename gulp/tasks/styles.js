38f2eefa4cba498ef9f273bc701ac5742c845e9e
Josh Miller <notjoshmiller@gmail.com>
2014-11-30 18:20:07 -0600

    Add plumber to LESS compile task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
 
 gulp.task('styles', function () {
   return gulp.src('src/styles/app.less')
+        .pipe(plumber())
         .pipe(less({
             paths: ['src/styles/']
         }))
