55569d801b3a59fe88e9d5eaf20b295a9faefdaa
Josh Miller <notjoshmiller@gmail.com>
2014-12-02 10:30:17 -0600

    Replace gulp-filter with gulp-if, update useref and build task to fix bower deps

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,24 +1,19 @@
 var gulp = require('gulp');
-var filter = require('gulp-filter');
+var gulpif = require('gulp-if');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var csso = require('gulp-csso');
 var plumber = require('gulp-plumber');
 
 gulp.task('build', ['browserify', 'styles', 'images'], function() {
-    var jsFilter = filter('**/*.js');
-    var cssFilter = filter('**/*.css');
+    var assets = useref.assets({ searchPath: ['./.tmp', './src'] });
 
     return gulp.src('./src/index.html')
         .pipe(plumber())
-        .pipe(useref.assets({ searchPath: ['./.tmp', './src'] }))
-        .pipe(jsFilter)
-        .pipe(uglify())
-        .pipe(jsFilter.restore())
-        .pipe(cssFilter)
-        .pipe(csso())
-        .pipe(cssFilter.restore())
-        .pipe(useref.restore())
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', csso()))
+        .pipe(assets.restore())
         .pipe(useref())
         .pipe(gulp.dest('./dist'));
 });
